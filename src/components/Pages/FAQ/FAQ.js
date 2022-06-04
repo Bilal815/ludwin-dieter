f578e048d1d20afde3cd7390658a001990c69eaf
Bilal815 <62904483+Bilal815@users.noreply.github.com>
2022-06-04 23:44:07 +0300

    Package.json push

--- src/components/Pages/FAQ/FAQ.js
+++ src/components/Pages/FAQ/FAQ.js
@@ -34,37 +34,37 @@ const FAQ = () => {
 <section className="faqs__section">
 <Accordion defaultActiveKey="0" flush>
   <Accordion.Item eventKey="0">
-    <Accordion.Header>Whatâ€™s the best way to contact Ludwin Dieter Customer service?</Accordion.Header>
+    <Accordion.Header>What’s the best way to contact Ludwin Dieter Customer service?</Accordion.Header>
     <Accordion.Body>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     </Accordion.Body>
   </Accordion.Item>
   <Accordion.Item eventKey="1">
-    <Accordion.Header>Whatâ€™s the best way to contact Ludwin Dieter Customer service?</Accordion.Header>
+    <Accordion.Header>What’s the best way to contact Ludwin Dieter Customer service?</Accordion.Header>
     <Accordion.Body>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     </Accordion.Body>
   </Accordion.Item>
   <Accordion.Item eventKey="2">
-    <Accordion.Header>Whatâ€™s the best way to contact Ludwin Dieter Customer service?</Accordion.Header>
+    <Accordion.Header>What’s the best way to contact Ludwin Dieter Customer service?</Accordion.Header>
     <Accordion.Body>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     </Accordion.Body>
   </Accordion.Item>
   <Accordion.Item eventKey="3">
-    <Accordion.Header>Whatâ€™s the best way to contact Ludwin Dieter Customer service?</Accordion.Header>
+    <Accordion.Header>What’s the best way to contact Ludwin Dieter Customer service?</Accordion.Header>
     <Accordion.Body>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     </Accordion.Body>
   </Accordion.Item>
   <Accordion.Item eventKey="4">
-    <Accordion.Header>Whatâ€™s the best way to contact Ludwin Dieter Customer service?</Accordion.Header>
+    <Accordion.Header>What’s the best way to contact Ludwin Dieter Customer service?</Accordion.Header>
     <Accordion.Body>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     </Accordion.Body>
   </Accordion.Item>
   <Accordion.Item eventKey="5">
-    <Accordion.Header>Whatâ€™s the best way to contact Ludwin Dieter Customer service?</Accordion.Header>
+    <Accordion.Header>What’s the best way to contact Ludwin Dieter Customer service?</Accordion.Header>
     <Accordion.Body>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     </Accordion.Body>
